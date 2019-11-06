72be3f7c29ffd8be1317756fd01faa3a2c353aea
EtesseGuillaume <guillaume22etesse@gmail.com>
2019-11-06 15:04:12 +0100

    added image  by theme + added csv library

--- api/routes/route.js
+++ api/routes/route.js
@@ -3,8 +3,10 @@ module.exports = function(app) {
     var image_controller = require('../controllers/image_controller');
 
   // Image routes
-    app.route('/Images/:id')
+    app.route('/Images/getById/:id')
         .get(image_controller.image_by_id);
+    app.route('/Images/getByTheme/:theme')
+        .get(image_controller.image_by_theme);
     app.route('/Images/')
         .get(image_controller.image_all);
 };
