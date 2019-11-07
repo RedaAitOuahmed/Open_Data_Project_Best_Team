2dd350cdbbea78f1d6296fcd7b6825a78df723b0
EtesseGuillaume <guillaume22etesse@gmail.com>
2019-11-07 13:02:03 +0100

    add json and csv format

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 3000
 var express = require('express')
 var csv = require('csv-express')
 var fs = require('file-system')
+var xmlparser = require('express-xml-bodyparser');
 var  app = express()
 
 
@@ -17,33 +18,8 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/route'); //importing route
 routes(app); //register the route
 
-app.get('/index', function(req,res) {
-	fs.readFile("./client/client.html", function(err, html) {
-	if(err){throw err;}
-	res.writeHead(200, {'Content-Type': 'text/html'})
-            res.write(html)
-            res.end()
-	})
-
-})
 
 app.listen(port);
-
-app.get('/names', function(req,res) {
-    res.format({
-        'application/json': function () {
-            res.json(resultat);
-        },
-
-        'application/csv': function () {
-            res.csv(resultat);
-        },
-
-        'application/xml': function () {
-            res.xml(resultat);
-        }
-    })
-});
 
 
 console.log('todo list RESTful API server started on: ' + port);
