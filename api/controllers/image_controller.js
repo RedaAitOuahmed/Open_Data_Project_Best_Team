5fa8baa996e059be28b68da41cec73d784b98ae4
EtesseGuillaume <guillaume22etesse@gmail.com>
2019-11-07 18:31:51 +0100

    added image function with csv

--- api/controllers/image_controller.js
+++ api/controllers/image_controller.js
@@ -15,10 +15,15 @@ exports.image_by_id = function(req, res) {
                 res.json([results]);
             },
             'application/csv': function () {
-                res.csv([results],true);
-            },
-            'application/xml': function () {
-                res.xmlparser([results]);
+                var con=[results]
+                var li_test = []
+                for (var i = 0; i< con.length ; i++) {
+                    var im ={}
+                    im.id=con[i].id
+                    im.largeImageURL=con[i].largeImageURL
+                    li_test.push(im)
+                }
+                res.csv(li_test,true);
             }
         })
          );
@@ -33,13 +38,30 @@ exports.image_by_id = function(req, res) {
 //     });
 };
 
+
 exports.image_by_theme = function(req, res) {
     // retrieve image theme from request
     var theme = req.params.theme;
     // call function get by theme of Image model class
     var results = Image.getByTheme(theme);
     // format the response according to user preferences (json, csv, xml ...)
-    Image.getByTheme(theme).then(results => res.json(results));
+    Image.getByTheme(theme).then(results =>
+        res.format({
+            'application/json': function () {
+                res.json([results]);
+            },
+            'application/csv': function () {
+                var li_test = []
+                for (var i = 0; i< results.length ; i++) {
+                    var im ={}
+                    im.id=results[i].id
+                    im.largeImageURL=results[i].largeImageURL
+                    li_test.push(im)
+                }
+                res.csv(li_test,true);
+            }
+        })
+    );
 
 };
 
